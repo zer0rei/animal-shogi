80d43ff620a6cb05020869b3b5ee83b039c68e38
Elgaoussi Hamza <rei-zer0@live.com>
2021-02-14 13:22:23 +0100

    Updates

---
+++ precache-manifest.320d3dfad8acefe3daeab7c765c42f8d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aff06f1c86ff99fa801fcac0c8598d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff3091cf008fea821672",
+    "url": "/static/css/main.8743cf17.chunk.css"
+  },
+  {
+    "revision": "02f0655194fa6fec1c41",
+    "url": "/static/js/2.5fcec6e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5fcec6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff3091cf008fea821672",
+    "url": "/static/js/main.5d9025e0.chunk.js"
+  },
+  {
+    "revision": "5e01898d72f20c278a18",
+    "url": "/static/js/runtime-main.29aef5fe.js"
+  },
+  {
+    "revision": "b40c0a33d6da693239b1e9aff72e52cb",
+    "url": "/static/media/bubblegum.b40c0a33.ttf"
+  },
+  {
+    "revision": "1595e17d06df1ee5c5d1715854c6f3c8",
+    "url": "/static/media/chick.1595e17d.svg"
+  },
+  {
+    "revision": "c2d34d4dab2f7d973353489a9f11c078",
+    "url": "/static/media/elephant.c2d34d4d.svg"
+  },
+  {
+    "revision": "70bb2a93cae288ecd3006fb0455e1ec4",
+    "url": "/static/media/giraffe.70bb2a93.svg"
+  },
+  {
+    "revision": "11a67dae3ddee83ced60af2dbb0cf00c",
+    "url": "/static/media/hen.11a67dae.svg"
+  },
+  {
+    "revision": "07f5bd3d38df5792b1f35da9bc176acb",
+    "url": "/static/media/lion.07f5bd3d.svg"
+  },
+  {
+    "revision": "f651b8d87bb6437f467f69e0e8414312",
+    "url": "/static/media/reset-icon.f651b8d8.svg"
+  }
+]);
